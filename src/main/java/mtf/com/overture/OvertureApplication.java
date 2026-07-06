a58c58f306189f98c8830a3f378d17f4cdd5ed64
siwol025 <cyk99115695@gmail.com>
2026-07-06 15:51:04 +0900

    chore: Spring Boot 4 프로젝트 초기 생성

---
+++ src/main/java/mtf/com/overture/OvertureApplication.java
@@ -0,0 +1,13 @@
+package mtf.com.overture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mtf/com/overture/OvertureApplicationTests.java
@@ -0,0 +1,13 @@
+package mtf.com.overture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
